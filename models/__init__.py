a2cde96f5e605410050684bdf325dea1fd62fcda
Natan <natan.sisoev@gmail.com>
2026-04-02 14:08:53 +0200

    Removed DenseSmallNet

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,9 @@
 from .smallresnet import SmallResNet
-from .densesmallnet import DenseSmallNet
 from .utils import count_parameters, plot_history, get_device
 from .train import train
 
 __all__ = [
     "SmallResNet",
-    "DenseSmallNet",
     "count_parameters",
     "plot_history",
     "get_device",
